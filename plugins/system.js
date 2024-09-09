ee4060aa744564489cfd291a7c4b538b252e6efa
vipshadow12 <shadowmdwatsapp@gmail.com>
2024-09-10 00:41:22 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,30 @@
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "check up time",
+    category: "main",
+    react: "📟",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{ 
+
+let status =`┌────────────────────
+    *├ ⏳ Uptime:-  ${runtime(process.uptime())}*    
+    *├ 📟 Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+    *├ ⚙️ HostName:- ${os.hostname()}*
+    *├ 👨‍💻 main Owner:- Shadow / Lakshan teach*   
+    *├ 🤖 Versions:- 2.0.1*
+    └───────────────────────
+    > ❯❯ ꜱʜᴀᴅᴏᴡ-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
